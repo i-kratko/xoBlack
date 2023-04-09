b2ee92c9ba172ff500fc6b4c26397e2092706bbe
Nikolay Yordanov <nikolay.i.yorddanov.2019@elsys-bg.org>
2023-04-09 18:33:36 +0300

    demo

--- Code/Module/Module.ino
+++ Code/Module/Module.ino
@@ -1,7 +1,34 @@
+#include <Arduino.h>
+#if defined(ESP32)
+  #include <WiFi.h>
+#elif defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#endif
+#include <ESP_Mail_Client.h>
 #include <SoftwareSerial.h>
 #include <TinyGPSPlus.h>
 #include "Wire.h"
 #include <MPU6050_light.h>
+
+//trust
+//500 promenlivi
+#define WIFI_SSID "A1_BDF6"
+#define WIFI_PASSWORD "48575443203A96AA"
+
+#define SMTP_HOST "smtp.gmail.com"
+#define SMTP_PORT 465
+
+/* The sign in credentials */
+#define AUTHOR_EMAIL "noreplyxoblack@gmail.com"
+#define AUTHOR_PASSWORD "aqghxsqozgzefmex"
+
+/* Recipient's email*/
+#define RECIPIENT_EMAIL "nikiyord1@gmail.com"
+
+/* The SMTP Session object used for Email sending */
+SMTPSession smtp;
+
+//trust
 
 MPU6050 mpu(Wire);
 TinyGPSPlus gps;
@@ -22,6 +49,9 @@ int accDetect = 0;
 
 int SMStx = 0;
 
+/* Callback function to get the Email sending status */
+void smtpCallback(SMTP_Status status);
+
 void setup() {
   pinMode(Sen, INPUT);
   pinMode(RLed, OUTPUT);
@@ -41,6 +71,63 @@ void setup() {
   delay(1000);
   mpu.calcOffsets(true,true); // gyro and accelero
   Serial.println("Done!\n");
+
+  //aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+  /** Enable the debug via Serial port
+   * none debug or 0
+   * basic debug or 1
+  */
+  smtp.debug(1);
+
+  /* Set the callback function to get the sending results */
+  smtp.callback(smtpCallback);
+
+  /* Declare the session config data */
+  ESP_Mail_Session session;
+
+  /* Set the session config */
+  session.server.host_name = SMTP_HOST;
+  session.server.port = SMTP_PORT;
+  session.login.email = AUTHOR_EMAIL;
+  session.login.password = AUTHOR_PASSWORD;
+  session.login.user_domain = "";
+
+  /* Declare the message class */
+  SMTP_Message message;
+
+  /* Set the message headers */
+  message.sender.name = "ESP";
+  message.sender.email = AUTHOR_EMAIL;
+  message.subject = "ESP Test Email";
+  message.addRecipient("Sara", RECIPIENT_EMAIL);
+
+  /*Send HTML message*/
+  String htmlMsg = "<div style=\"color:#2f4468;\"><h1>Hello World!</h1><p>- Sent from ESP board</p></div>";
+  message.html.content = htmlMsg.c_str();
+  message.html.content = htmlMsg.c_str();
+  message.text.charSet = "us-ascii";
+  message.html.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
+
+  /*
+  //Send raw text message
+  String textMsg = "Hello World! - Sent from ESP board";
+  message.text.content = textMsg.c_str();
+  message.text.charSet = "us-ascii";
+  message.text.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
+  
+  message.priority = esp_mail_smtp_priority::esp_mail_smtp_priority_low;
+  message.response.notify = esp_mail_smtp_notify_success | esp_mail_smtp_notify_failure | esp_mail_smtp_notify_delay;*/
+
+  /* Set the custom message header */
+  //message.addHeader("Message-ID: <abcde.fghij@gmail.com>");
+
+  /* Connect to server with the session config */
+  if (!smtp.connect(&session))
+    return;
+
+  /* Start sending Email and close the session */
+  if (!MailClient.sendMail(&smtp, &message))
+    Serial.println("Error sending Email, " + smtp.errorReason());
   
 }
 
@@ -57,6 +144,7 @@ void loop() {
  
     if(analogRead(Sen) > 2500)
     {
+      Serial.println("Mic detect");
       micDetect = 1;
       x++;
       delay(1000);
@@ -71,11 +159,13 @@ void loop() {
     delay(1000);
   }
    if (mpu.getAccY() > 1 || mpu.getAccY() < -1) {
+    Serial.println("Gyros detect");
     accDetect = 1;
     x++;
     delay(1000);
   }
   if (mpu.getAccZ() > 1.1 || mpu.getAccZ() < -1) {
+    Serial.println("Gyros detect");
     accDetect = 1;
     x++;
     delay(1000);
@@ -123,4 +213,32 @@ void loop() {
     timer = millis();
   }
   //countdown--;
+}
+
+void smtpCallback(SMTP_Status status){
+  /* Print the current status */
+  Serial.println(status.info());
+
+  /* Print the sending result */
+  if (status.success()){
+    Serial.println("----------------");
+    ESP_MAIL_PRINTF("Message sent success: %d\n", status.completedCount());
+    ESP_MAIL_PRINTF("Message sent failled: %d\n", status.failedCount());
+    Serial.println("----------------\n");
+    struct tm dt;
+
+    for (size_t i = 0; i < smtp.sendingResult.size(); i++){
+      /* Get the result item */
+      SMTP_Result result = smtp.sendingResult.getItem(i);
+      time_t ts = (time_t)result.timestamp;
+      localtime_r(&ts, &dt);
+
+      ESP_MAIL_PRINTF("Message No: %d\n", i + 1);
+      ESP_MAIL_PRINTF("Status: %s\n", result.completed ? "success" : "failed");
+      ESP_MAIL_PRINTF("Date/Time: %d/%d/%d %d:%d:%d\n", dt.tm_year + 1900, dt.tm_mon + 1, dt.tm_mday, dt.tm_hour, dt.tm_min, dt.tm_sec);
+      ESP_MAIL_PRINTF("Recipient: %s\n", result.recipients.c_str());
+      ESP_MAIL_PRINTF("Subject: %s\n", result.subject.c_str());
+    }
+    Serial.println("----------------\n");
+  }
 }
